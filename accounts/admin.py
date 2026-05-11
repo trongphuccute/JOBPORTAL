e98ad41a4fe24ccbacfb077c1d661c9563341977
trongphuccute <phuc13032005@gmail.com>
2026-05-11 11:24:06 +0700

    fix: update company for employer

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import EmployerRequest, User, Profile
+from .models import EmployerRequest, User, Profile, Company
 from .utils import send_employer_approved_email
+
 
 admin.site.register(User)
 admin.site.register(Profile)
 
+
 @admin.register(EmployerRequest)
 class EmployerRequestAdmin(admin.ModelAdmin):
     list_display = ('user', 'company_name', 'status')
-
     actions = ['approve']
 
     def approve(self, request, queryset):
@@ -19,6 +20,14 @@ class EmployerRequestAdmin(admin.ModelAdmin):
             user = req.user
             user.role = 'employer'
             user.save()
+
+            # 🔥 TẠO COMPANY (PHẦN BẠN ĐANG THIẾU)
+            Company.objects.get_or_create(
+                user=user,
+                defaults={
+                    "name": req.company_name,
+                }
+            )
 
             # 🔥 GỬI EMAIL
             send_employer_approved_email(user)
