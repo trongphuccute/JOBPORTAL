252e553ba136743d86044b85d44559fe0e412305
trongphuccute <phuc13032005@gmail.com>
2026-05-11 11:28:17 +0700

    fix: update company for employer 01

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import EmployerRequest, User, Profile, Company
+from .models import EmployerRequest, User, Profile
 from .utils import send_employer_approved_email
+from jobs.models import Company
 
 
 admin.site.register(User)
