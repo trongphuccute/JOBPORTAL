991aa90907a9d7aaf18e4b873f8a3f6a92ce0094
trongphuccute <phuc13032005@gmail.com>
2026-05-12 15:30:28 +0700

    fix: change name email assync and change setting 52

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -104,15 +104,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = '/accounts/login/'
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
 EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = config("SENDGRID_API_KEY")
+SENDGRID_API_KEY = config("SENDGRID_API_KEY")
 
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
